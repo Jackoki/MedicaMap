3d27a28123fb30183fb6a5facbeb20f9f647a028
Kenji Inoue <kenji.inoue@hotmail.com>
2026-09-17 22:36:58 -0300

    Alteração do gitignore

---
+++ MedicaMap/Data/MedicaMapContext.cs
@@ -0,0 +1,25 @@
+﻿using MedicaMap.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicaMap.Data
+{
+    public class MedicaMapContext : DbContext
+    {
+        public MedicaMapContext(DbContextOptions<MedicaMapContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Municipality> Municipalities { get; set; }
+        public DbSet<Establishment> Establishments { get; set; }
+        public DbSet<Medication> Medications { get; set; }
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<State> States { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Municipality>().HasOne(m => m.State).WithMany(s => s.Municipalities).HasForeignKey(m => m.StateId).OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
